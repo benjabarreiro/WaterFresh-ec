812b020b478e5363acd43b19efda3bb90c5efe57
barreirobenjamin@gmail.com <barreirobenjamin@gmail.com>
2021-03-04 21:58:45 -0300

    Database connection

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,26 +1,26 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "waterFresh_db",
-    "host": "127.0.0.1",
+    "username": "bf6b6448175c39",
+    "password": "d9967d2b",
+    "database": "heroku_729ca7676ec025d",
+    "host": "us-cdbr-east-03.cleardb.com",
     "port": 8889,
     "dialect": "mysql",
     "timezone": "-03:00"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "bf6b6448175c39",
+    "password": "d9967d2b",
+    "database": "heroku_729ca7676ec025d",
+    "host": "us-cdbr-east-03.cleardb.com",
     "dialect": "mysql",
     "timezone": "-03:00"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "bf6b6448175c39",
+    "password": "d9967d2b",
+    "database": "heroku_729ca7676ec025d",
+    "host": "us-cdbr-east-03.cleardb.com",
     "dialect": "mysql",
     "timezone": "-03:00"
   }
