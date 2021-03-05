12c71cf97ae35bbc62c7aff5cac9b3a865ec2645
barreirobenjamin@gmail.com <barreirobenjamin@gmail.com>
2021-03-04 23:05:10 -0300

    db v8

--- src/app.js
+++ src/app.js
@@ -49,4 +49,6 @@ app.use('/users', usersRouter);
 app.use('/distributors', distributorsRouter);
 app.use('/admins', adminsRouter);
 
-app.listen(3000, () => console.log('Server running in port 3000'));
+const port = 3306 || 3000
+
+app.listen(port, () => console.log('Server running in port 3000'));
