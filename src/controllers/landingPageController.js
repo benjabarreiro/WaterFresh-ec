b74e908f6057ab9b2aea32446d5739e342869149
barreirobenjamin@gmail.com <barreirobenjamin@gmail.com>
2021-03-11 17:00:33 -0300

    landingpage json

--- src/controllers/landingPageController.js
+++ src/controllers/landingPageController.js
@@ -1,4 +1,6 @@
 const db = require('../database/models');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     landingPage: function(req, res) {
@@ -10,6 +12,17 @@ module.exports = {
             });
     },
     submit: function(req, res) {
+
+        let landingPage = {
+            email: req.body.email,
+            name: req.body.name,
+            phone: req.body.phone
+        }
+
+        let landingPageJSON = JSON.stringify(landingPage);
+
+        fs.appendFileSync('landingPage.json', landingPageJSON);
+
         db.LandingPage.create({
             email: req.body.email,
             name: req.body.name,
